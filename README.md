d3f82c6aa29375c447ad5df9b5c212d59980370e
bhadreshdesai <bhadresh_desai@hotmail.com>
2020-01-31 17:17:26 +0000

    1. Import initial source

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
+[![Build Status](https://travis-ci.org/bhadreshdesai/SpringVaadinApp.svg?branch=develop)](https://travis-ci.org/bhadreshdesai/SpringVaadinApp)
+[![codecov](https://codecov.io/gh/bhadreshdesai/SpringVaadinApp/branch/develop/graph/badge.svg)](https://codecov.io/gh/bhadreshdesai/SpringVaadinApp)
+
+
 # SpringVaadinApp
---
+++ src/main/java/bdd/demo/genapp/SpringVaadinAppApplication.java
@@ -0,0 +1,13 @@
+package bdd.demo.genapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringVaadinAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringVaadinAppApplication.class, args);
+	}
+
+}
